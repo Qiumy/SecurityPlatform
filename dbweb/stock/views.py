446bbb1021aea11fb433d5deca48317d299630a5
qiumy <1415456511@qq.com>
2016-11-03 21:16:41 +0800

    修复合并冲突

--- dbweb/stock/views.py
+++ dbweb/stock/views.py
@@ -7,18 +7,28 @@ from flask_babel import gettext
 import tushare as ts, datetime, json
 DELTA_DAYS = 360
 
-@stock.route('/<code>')
-def index(code):
+# @stock.route("/")
+# def index():
+# 	pass
+
+@stock.route('/allstock/')
+def allstock():
+	whole_indicators = json.loads(ts.get_index().to_json(orient='records'))
+	return render_template('stock/all_stock.html',
+			whole_indicators=whole_indicators)
+
+@stock.route('/<code>/')
+def stock_view(code):
     with current_app.open_resource('stock.json') as f:
         stock_dic = json.loads(f.read())
-    
+
     end = datetime.datetime.now().date()
     start = end - datetime.timedelta(days=DELTA_DAYS)
     stock_data = json.loads(ts.get_hist_data(code=code,start=str(start),end=str(end)).to_json(orient='split'))
     name = stock_dic[code]
     data_days_order = range(len(stock_data['index']))[::-1]
 
-    return render_template('stock/index.html',
+    return render_template('stock/stock_detail.html',
                             title=u'个股详情-'+name,
                             code=code,
                             name=name,
