1026b6fb05233ac88e3555b54d4421db9981a861
qiumy <1415456511@qq.com>
2016-11-05 15:36:59 +0800

    更新SQL & 修正错误

--- dbweb/admin/views.py
+++ dbweb/admin/views.py
@@ -19,14 +19,12 @@ def auth():
     elif request.method == "POST":
         _form = request.form
         u = User.query.filter_by(email=_form['email']).first()
-        if u and u.verify_password(_form['password']) and (u.permissions == 0 or u.permissions == 2):
+        if u and u.verify_password(_form['password']) and (u.permissions == 0):
             login_user(u)
             u.last_login = datetime.now()
             db.session.commit()
             if u.permissions == 0:
                 return redirect(url_for('admin.system'))
-            else:
-                return redirect(url_for('admin.teacher'))
         else:
             message = gettext('Invalid username or password.')
             return render_template('admin/auth.html', title=u"管理员认证",
